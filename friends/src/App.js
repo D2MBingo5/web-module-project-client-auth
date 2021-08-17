41c962f43f205fcb582d141b2cad524e74e8269c
Leu Family <romanianfobjr@gmail.com>
2021-08-17 17:17:38 -0500

    logout deletes token, protected nav element conditionally renders

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,14 +4,27 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 import Login from './components/Login'
+import Logout from './components/Logout';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <h1>Friends</h1>
+        <nav>
+          <Link to='/login'>Login</Link>
+          <span>----</span>
+          <Link to='/logout'>Logout</Link>
+          {localStorage.getItem('token') && 
+          <span>----</span>
+          }
+          {localStorage.getItem('token') && 
+          <Link to='/'>Protected</Link>
+          }
+        </nav>
 
         <Switch>
+          <Route path='/logout' component={Logout}/>
           <Route path='/login' component={Login}/>
           <Route path='/' component={Login}/>
         </Switch>
